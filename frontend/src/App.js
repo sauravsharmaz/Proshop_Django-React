878ef1da101f2aad8d3ed19d3487c5be720a38bc
Saurav Sharma <saurav.sharma.zz45@gmail.com>
2021-06-03 13:11:05 +0530

    added navbar & footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+
 
 function App() {
   return (
     <div>
-          Learn React
-
+      <Header />
+      <main>
+      <Container>
+          Learn React with me
+      </Container>
+      </main>
+      <Footer />
     </div>
   );
 }

fe10da120c0f7eb046e1ab66b83cf8883215afff
Saurav Sharma <saurav.sharma.zz45@gmail.com>
2021-06-13 12:45:00 +0530

    added product component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import {Card } from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 
-function Product({product}){
-    return(
+function Product({ product }) {
+    return (
         <div>
-        <Card.Img src={product.image} />
-        <h2>{product.title} </h2><p>{product.brand}</p><hr />
-        <Card.Text>{product.desc}</Card.Text>
-        <h2>${product.price}</h2>
+            <Card.Img src={product.image} />
+            <h2>{product.title} </h2><p>{product.brand}</p><hr />
+            <Card.Text>{product.desc}</Card.Text>
+            <h2>${product.price}</h2>
         </div>
     );
 }
